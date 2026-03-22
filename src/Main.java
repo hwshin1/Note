e968858cc52eb7f8efe5753c4e80eed55e9983d9
hwshin1 <hwshin5044@gmail.com>
2026-03-22 21:18:59 +0900

    배열 만들기 2(못풀음)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // {시전시간, 초당 회복량, 추가 회복량}
-        int[] bandage = {5, 1, 5};
-        // 최대 체력
-        int health = 30;
-        // {{공격시간, 피해량}}
-        int[][] attacks = {{2, 1}, {9, 15}, {10, 5}, {11, 5}};
+       int l = 5;
+       int r = 555;
+       int[] answer = {};
 
-        for (int i = 0; i < attacks.length; i++) {
-            health = health - attacks[i][1];
-        }
-
-        System.out.println(health);
+       if ((r % 2 == 1) && (l % 2 == 1)) {
+           answer = new int[]{l, r};
+           System.out.println(Arrays.toString(answer));
+       } else  {
+           answer = new int[]{-1};
+           System.out.println(Arrays.toString(answer));
+       }
     }
 }
